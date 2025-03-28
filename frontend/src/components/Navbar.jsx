d853f9a8b65988c7131fe5b1965a9c6230e026eb
Dev Harsh <devharsh213@gmail.com>
2025-03-29 04:40:12 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import logo from '../assets/Logo.png'
+import logo from '../assets/logo.png'
 import { fadeIn } from '../utils/motion';
 import { motion } from 'framer-motion';
 import { withLoadTracking } from './withLoadTracking'
